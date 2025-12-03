7df9f398353baf55a56f1addae68f3dd67d4465f
ansh <anshpay@icloud.com>
2025-12-03 13:40:01 +0000

    Add Vite React/TS frontend with booking wizard and Netlify config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookingWizard from './components/BookingWizard';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <BookingWizard />
+    </div>
+  );
+};
+
+export default App;
